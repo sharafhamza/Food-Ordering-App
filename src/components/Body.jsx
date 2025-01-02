08173d49be7ee1274a8536380a6d2488b1227e0f
sharafhamza <kingsharaf14@gmail.com>
2025-01-02 23:22:46 +0600

    added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -8,6 +8,9 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
     const json = await data.json();
     const newData =
       json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
