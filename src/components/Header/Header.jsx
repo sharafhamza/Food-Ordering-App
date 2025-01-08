8e4e052f902dba0be8c720c9331372eb3b2ded4b
sharafhamza <kingsharaf14@gmail.com>
2025-01-08 17:55:34 +0600

    font added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,13 @@ import Navbar from "./Navbar";
 import { LOGO } from "../../utils/constant";
 const Header = () => {
   return (
-    <div>
-      <img src={LOGO} alt="Logo" />
+    <div className="w-[90%] mx-auto">
+      <div className="w-20">
+        <img src={LOGO} alt="Logo" />
+      </div>
+      <div>
+        <span className="inline-block border-b-2 border-black">Other</span>
+      </div>
       <Navbar />
     </div>
   );
