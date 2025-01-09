f13ba65157bfe3e586cf0e5642b5cc9231ed83fe
sharafhamza <kingsharaf14@gmail.com>
2025-01-09 11:08:37 +0600

    font change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { IoIosArrowDown } from "react-icons/io";
+
 import { LOGO } from "../../utils/constant";
 const Header = () => {
   return (
-    <div className="w-[90%] mx-auto font-custom">
-      <div className="w-20">
-        <img src={LOGO} alt="Logo" />
+    <div className="shadow-lg shadow-[#ddd]/40">
+      <div className="w-[90%] mx-auto font-custom flex items-center justify-between ">
+        <div className="flex gap-8 py-4 ">
+          <div className="w-20">
+            <img src={LOGO} alt="Logo" />
+          </div>
+          <div className="flex items-center justify-between w-[70px] cursor-pointer">
+            <span className="inline-block border-b-2 font-bold text-sm border-black ">
+              Other
+            </span>
+            <IoIosArrowDown className="text-orange-500 text-[18px]" />
+          </div>
+        </div>
+        <Navbar />
       </div>
-      <div>
-        <span className="inline-block border-b-2 border-black ">Other</span>
-      </div>
-      <Navbar />
     </div>
   );
 };
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { RiSearchLine } from "react-icons/ri";
+import { BiSolidOffer } from "react-icons/bi";
+import { IoHelpBuoyOutline } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa";
+import { IoMdCart } from "react-icons/io";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navItems = [
+    {
+      name: "Swiggy Corporate",
+      icon: HiOutlineShoppingBag,
+    },
+    {
+      name: "Search",
+      icon: RiSearchLine,
+    },
+    {
+      name: "Offer",
+      icon: BiSolidOffer,
+    },
+    {
+      name: "Help",
+      icon: IoHelpBuoyOutline,
+    },
+    {
+      name: "Sign In",
+      icon: FaRegUser,
+    },
+    {
+      name: "Cart",
+      icon: IoMdCart,
+    },
+  ];
+  return (
+    <ul className="flex">
+      {navItems.map((item, index) => {
+        const IconComponent = item.icon;
+        return (
+          <li key={index} className="flex items-center text-black">
+            <IconComponent size={18} />
+            <span className="text-base font-medium">{item.name}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Navbar;
