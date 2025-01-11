0770296608fa18f147970f96c91e9fe87fb60d83
sharafhamza <kingsharaf14@gmail.com>
2025-01-11 23:08:25 +0600

    changs

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+
 import OnMindSlider from "./onMindSlider/OnMindSlider";
 const fetchData = async () => {
   const [resInfo, setResInfo] = useState([]);
