bb22d94d5e0f28e0863b170f39bba7a22d80edea
sharafhamza <kingsharaf14@gmail.com>
2025-04-30 23:36:17 +0600

    full completed

--- src/components/card/CardSingle.jsx
+++ src/components/card/CardSingle.jsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Single_Url, Image_Url } from "../../utils/constant"; // Assuming you have Image_Url
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useRestaurentData from "../hooks/useRestaurentData";
-import { GiRoundStar } from "react-icons/gi";
-import { GoTriangleDown } from "react-icons/go";
 
+import CardTopDetails from "./CardTopDetails";
 const CardSingle = () => {
   const { resId } = useParams();
 
@@ -28,56 +26,7 @@ const CardSingle = () => {
     costForTwoMessage,
   } = resInfo?.cards[2].card?.card?.info || {};
 
-  return (
-    <div className="single-box max-w-4xl mx-auto mt-6 p-5 rounded-2xl">
-      <div class="mx-auto border-2 bg-white rounded-2xl  p-5 flex flex-col space-y-3 ">
-        <h2 class="text-2xl font-bold">Chinese Wok</h2>
-
-        <div class="flex items-center space-x-2 text-sm text-black font-gilroy font-bold">
-          <div class="flex items-center space-x-1  font-semibold">
-            <span className="font-bold">
-              <span className="flex items-center gap-x-2 text-base">
-                <div className="bg-green-600 w-5 h-5 rounded-full flex items-center justify-center">
-                  <GiRoundStar className="text-white z-10 text-sm" />
-                </div>
-                4.3 (734 ratings)
-              </span>
-            </span>
-          </div>
-          <span class="text-gray-400">•</span>
-          <span>₹400 for two</span>
-        </div>
-
-        <div class="text-sm text-orange-600 font-bold pb-2">
-          <a href="#" class="hover:underline">
-            Burgers, Biryani
-          </a>
-        </div>
-
-        <div class="flex items-center space-x-2 text-sm text-black ">
-          <div class="flex items-center gap-2 h-5">
-            <div className="flex flex-col justify-center w-2">
-              <div class="w-[7px] h-[7px] mx-auto rounded-full bg-gray-400 mr-2"></div>
-              <div className="w-[1px] mx-auto h-6 bg-gray-400"></div>
-              <div class="w-[7px] h-[7px] mx-auto rounded-full bg-gray-400 mr-2"></div>
-            </div>
-            <div className="flex flex-col gap-1">
-              <div>
-                <span className="font-semibold flex gap-2">
-                  Outlet
-                  <span className="flex items-center text-gray-500">
-                    KOLKATA MUNICIPAL CORPORATION
-                    <GoTriangleDown className="text-orange-600" />
-                  </span>
-                </span>
-              </div>
-              <span className="font-semibold">40-45 mins</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <CardTopDetails />;
 };
 
 export default CardSingle;
---
+++ src/components/card/CardTopDetails.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { GiRoundStar } from "react-icons/gi";
+import { GoTriangleDown } from "react-icons/go";
+const CardTopDetails = () => {
+  return (
+    <>
+      <div className="single-box max-w-4xl mx-auto mt-6 p-5 rounded-3xl">
+        <div className="mx-auto border-2 bg-white rounded-3xl  p-5 flex flex-col space-y-3 ">
+          <h2 className="text-2xl font-bold">Chinese Wok</h2>
+
+          <div class="flex items-center space-x-2 text-sm text-black font-gilroy font-bold">
+            <div class="flex items-center space-x-1  font-semibold">
+              <span className="font-bold">
+                <span className="flex items-center gap-x-2 text-base">
+                  <div className="bg-green-600 w-5 h-5 rounded-full flex items-center justify-center">
+                    <GiRoundStar className="text-white z-10 text-sm" />
+                  </div>
+                  4.3 (734 ratings)
+                </span>
+              </span>
+            </div>
+            <span className="text-gray-400">•</span>
+            <span>₹400 for two</span>
+          </div>
+
+          <div className="text-sm text-orange-600 font-bold pb-2">
+            <a href="#" className="hover:underline">
+              Burgers, Biryani
+            </a>
+          </div>
+
+          <div class="flex items-center space-x-2 text-sm pb-2 text-black ">
+            <div class="flex items-center gap-2 h-5">
+              <div className="flex flex-col justify-center w-2">
+                <div class="w-[7px] h-[7px] mx-auto rounded-full bg-gray-400 mr-2"></div>
+                <div className="w-[1px] mx-auto h-6 bg-gray-400"></div>
+                <div class="w-[7px] h-[7px] mx-auto rounded-full bg-gray-400 mr-2"></div>
+              </div>
+              <div className="flex flex-col gap-1">
+                <div>
+                  <span className="font-semibold flex gap-2">
+                    Outlet
+                    <span className="flex items-center text-gray-500">
+                      KOLKATA MUNICIPAL CORPORATION
+                      <GoTriangleDown className="text-orange-600" />
+                    </span>
+                  </span>
+                </div>
+                <span className="font-semibold">40-45 mins</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      ;
+    </>
+  );
+};
+
+export default CardTopDetails;
